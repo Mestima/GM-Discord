c25c198dc4bc0beba1e7bef3600f68621da57a4b
Alex <mestima@icloud.com>
2019-04-12 13:38:58 +0300

    Web part upload
    There are redirection PHP scripts to redirect Garry's Mod GET and POST requests to the Discord API. CloudFlare that uses Discord do not allows GMod browser to send GET and POST requests that is the reason you must use redirection. Just upload this files to your web-server and configure api links at /lua/autorun/server/discord.lua

---
+++ web/send.php
@@ -0,0 +1,29 @@
+<?php
+/*
+	COPYRIGHT:
+		Made by Mestima © 2019
+	
+		If you're reading this at the GitHub, you need to know
+		that this script is licensed under the GPLv3 License (https://www.gnu.org/licenses/gpl-3.0.html)
+		Copyright removing is NOT allowed!
+		
+		http://steamcommunity.com/id/mestima
+		http://github.com/Mestima
+*/
+
+	if (isset($_POST["content"]) != true || isset($_POST["webhook"]) != true ||
+		$_POST["content"] == "" || $_POST["webhook"] == "") {
+		exit();
+	}
+	
+	$options = array(
+		'http' => array(
+			'header'  => "Content-type: application/json\r\n",
+			'method'  => 'POST',
+			'content' => $_POST["content"]
+		)
+	);
+	$context  = stream_context_create($options);
+
+	$result = file_get_contents($_POST["webhook"], false, $context);
+?>
